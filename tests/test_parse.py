fd8508ae1c29b82f2ddaf57a12e92374636b0253
Masum Billal <proafxin@users.noreply.github.com>
2024-07-02 19:00:35 +0600

    fixed coverage bug with opencv

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -12,7 +12,8 @@ cwd = dirname(abspath(__file__))
 @pytest.mark.asyncio
 async def test_parse() -> None:
     image_path = join(cwd, "1.jpg")
-    result = await parse_image(image_path=image_path, reader=Reader(lang_list=["en"]))
+    reader = Reader(lang_list=["en"])
+    result = await parse_image(image_path=image_path, reader=reader)
 
     assert isinstance(result, list)
 
@@ -31,3 +32,7 @@ async def test_parse() -> None:
 
         assert isinstance(text, str)
         assert isinstance(confidence, float)
+
+    image_path = join(cwd, "2.jpg")
+    with pytest.raises(FileNotFoundError):
+        await parse_image(image_path=image_path, reader=reader)
